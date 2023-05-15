5ca711c8a1ebedbec6a13383724308fed1a5e393
Nico <nicodiazan@gmail.com>
2023-05-15 20:28:02 +0200

    Improve shuffle algorithm

--- src/components/simon/Board.jsx
+++ src/components/simon/Board.jsx
@@ -4,14 +4,18 @@ import { useState } from "react";
 
 import Cell from "./Cell";
 
+import shuffle from "../../logic/shuffle";
+
 const Board = ({ size }) => {
 	const [isActive, setIsActive] = useState(true);
 
 	const colors = ["#55CB77", "#DA4642", "#E8E55A", "#0592E2"];
 
+    const shuffled = shuffle(colors);
+
 	return (
 		<section>
-			{colors.map((color, index) => {
+			{shuffled.map((color, index) => {
 				return (
 					<div key={index} style={{ backgroundColor: color }}>
 						Cell nº {index}
--- src/logic/shuffle.js
+++ src/logic/shuffle.js
@@ -1,21 +1,21 @@
 //This follows the Knuth algorithm for shuffling an array
 
 function shuffle(arr) {
-	let result = [],
-		len = arr.length,
-		i;
-
-	while (len) {
-		i = Math.floor(Math.random() * arr.length);
-
-		if (i in arr) {
-			result.push(arr[i]);
-			delete arr[i];
-			len--;
-		}
-	}
-
-	return result;
-}
+    var old = arr.length, curr, i;
+  
+    // While there remain elements to shuffle…
+    while (old) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * old--);
+  
+      // And swap it with the current element.
+      curr = arr[old];
+      arr[old] = arr[i];
+      arr[i] = curr;
+    }
+  
+    return arr;
+  }
 
 export default shuffle;
