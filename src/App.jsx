18e8deca85c26b316bf1a267ddd4564fa5adfe8e
Nico <nicodiazan@gmail.com>
2023-05-15 19:31:20 +0200

    Add skeleton for the first game

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./App.css";
 
+//This is wrong, but it's this way for testing purposes
+import Home from "./games/Simon";
+
 function App() {
 
 	return (
 		<Router>
 			<div>Hi</div>
 			<Routes>
-				<Route path="/" element={<div>Home</div>} />
+				<Route path="/" element={<Home />} />
 				<Route path="/task/:id" element={<div>This is a test</div>} />
 			</Routes>
 		</Router>
---
+++ src/games/Simon.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Simon = () => {
+  return (
+    <>
+    <div>Simon</div>
+    <p>Weee, this is the SImon game!</p>
+    </>
+
+  )
+}
+
+export default Simon
