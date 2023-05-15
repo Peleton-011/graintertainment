bf77a6def3c6891f85ef6b2881bd24fbe215a730
Nico <nicodiazan@gmail.com>
2023-05-15 19:26:30 +0200

    Remove some unnecessary boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <Router>
-        <div>Hi</div>
-        <Routes>
-        <Route path="/task/:id" element={<div>This is a test</div>} />
-
-        </Routes>
-    </Router>
-  )
+	return (
+		<Router>
+			<div>Hi</div>
+			<Routes>
+				<Route path="/" element={<div>Home</div>} />
+				<Route path="/task/:id" element={<div>This is a test</div>} />
+			</Routes>
+		</Router>
+	);
 }
 
-export default App
+export default App;
